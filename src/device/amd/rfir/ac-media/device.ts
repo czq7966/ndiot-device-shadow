827b8f58e845dd069997d0820ceabbe5427b7b7f
陈志谦 <761208@nd.com>
2022-12-16 01:26:24 +0800

    improve ac midea flow

--- src/device/amd/rfir/ac-media/device.ts
+++ src/device/amd/rfir/ac-media/device.ts
@@ -1,5 +1,5 @@
 
-import { IDeviceBusDataPayload, IDeviceBusDataPayloadHd } from "../../../device.dts";
+import { IDeviceBusDataPayload, IDeviceBusDataPayloadHd, IDeviceBusEventData } from "../../../device.dts";
 import { CmdId } from "../../coders/dev-bin-json/cmd";
 import { ICmdHead } from "../../coders/dev-bin-json/cmd-head";
 import { IPldTable, PldTable } from "../../coders/dev-bin-json/pld-table";
@@ -20,77 +20,59 @@ export  class RFIRDeviceACMedia extends RFIRDevice implements IRFIRDeviceACMedia
         Debuger.Debuger.log("RFIRDeviceACMedia init", this.attrs.id);
         this.ac_coder = new Coder();    
         this.rfir_coder = this.ac_coder.rfir_coder;    
-        // const param1 = new SegCoderParam();
-        // param1.tolerance = 20;
-        // param1.excess = 0;
-        // param1.atleast = true;                              
-        // param1.MSBfirst = true;
-        // param1.step = 2;
-        
-        // param1.nbits = TableConst.NBits;
-        // param1.headermark = TableConst.HeadMark;
-        // param1.headerspace = TableConst.HeadSpace;
-        // param1.onemark = TableConst.OneMark;
-        // param1.onespace = TableConst.OneSpace;
-        // param1.zeromark = TableConst.ZeroMark;
-        // param1.zerospace = TableConst.ZeroSpace;
-        // param1.footermark = TableConst.FooterMark;
-        // param1.footerspace = TableConst.FooterSpace;
-        // param1.lastspace = 0;        
-        // this.rfir_coder.params.push(param1);
         
     }
 
-    on_south_input_decode(p_hd: ICmdHead, p_pld: IPldTable): IDeviceBusDataPayload {
-        Debuger.Debuger.log("RFIRDeviceACMedia on_south_input_decode");
-        const payload = super.on_south_input_decode(p_hd, p_pld);
-        if (!payload) return;
+    // _on_south_input_decode(p_hd: ICmdHead, p_pld: IPldTable): IDeviceBusDataPayload {
+    //     Debuger.Debuger.log("RFIRDeviceACMedia on_south_input_decode");
+    //     const payload = super.on_south_input_decode(p_hd, p_pld);
+    //     if (!payload) return;
 
-        const hd = payload.hd;
-        let pld = payload.pld;
-        if (hd.cmd_id == CmdId.rfir_sniff) {  
-            const data = pld[PldTable.Keys.rfir_sniff_data];
-            if (data) {
-                if (this.ac_coder.pnt_table.decodeBytess(data)) {
-                    this.ac_coder.plf_props.decode(this.ac_coder.pnt_table);
-                    pld = this.ac_coder.plf_props.props;
-                }
-            }
-        } 
+    //     const hd = payload.hd;
+    //     let pld = payload.pld;
+    //     if (hd.cmd_id == CmdId.rfir_sniff) {  
+    //         const data = pld[PldTable.Keys.rfir_sniff_data];
+    //         if (data) {
+    //             if (this.ac_coder.pnt_table.decodeBytess(data)) {
+    //                 this.ac_coder.plf_props.decode(this.ac_coder.pnt_table);
+    //                 pld = this.ac_coder.plf_props.props;
+    //             }
+    //         }
+    //     } 
 
-        return {
-            hd: hd,
-            pld: pld
-        };       
-    }
+    //     return {
+    //         hd: hd,
+    //         pld: pld
+    //     };       
+    // }
   
-    on_north_input_encode(p_hd: IDeviceBusDataPayloadHd, p_pld: {}): IDeviceBusDataPayload {
-        const payload = super.on_north_input_encode(p_hd, p_pld);
-        if (!payload) return;
-        const hd = payload.hd;
-        const pld = payload.pld || {};
+    // _on_north_input_encode(p_hd: IDeviceBusDataPayloadHd, p_pld: {}): IDeviceBusDataPayload {
+    //     const payload = super.on_north_input_encode(p_hd, p_pld);
+    //     if (!payload) return;
+    //     const hd = payload.hd;
+    //     const pld = payload.pld || {};
 
-        if (hd.cmd_id == CmdId.set && !pld[PldTable.Keys.rfir_send_data] ) {  
-            const pnttable = this.ac_coder.plf_props.encode(pld, this.ac_coder.pnt_table);
-            pnttable.checksum()
+    //     if (hd.cmd_id == CmdId.set && !pld[PldTable.Keys.rfir_send_data] ) {  
+    //         const pnttable = this.ac_coder.plf_props.encode(pld, this.ac_coder.pnt_table);
+    //         pnttable.checksum()
             
             
-            const bytess = pnttable.encodeBytess();
-            const buf = this.rfir_coder.encode(bytess);
+    //         const bytess = pnttable.encodeBytess();
+    //         const buf = this.rfir_coder.encode(bytess);
             
-            // buf = Buffer.concat([buf, buf]);
+    //         // buf = Buffer.concat([buf, buf]);
 
-            if (buf && buf.length > 0) {
-                //须发两遍才生效
-                pld[PldTable.Keys.rfir_send_repeat] = 1;
-                pld[PldTable.Keys.rfir_send_data] = buf;   
-            }
-        }    
+    //         if (buf && buf.length > 0) {
+    //             //须发两遍才生效
+    //             pld[PldTable.Keys.rfir_send_repeat] = 1;
+    //             pld[PldTable.Keys.rfir_send_data] = buf;   
+    //         }
+    //     }    
         
-        return {
-            hd: hd,
-            pld: pld
-        }
+    //     return {
+    //         hd: hd,
+    //         pld: pld
+    //     }
 
-    }  
+    // }  
 }
--- src/device/amd/rfir/device.ts
+++ src/device/amd/rfir/device.ts
@@ -1,8 +1,4 @@
 
-import { IDeviceBusDataPayload, IDeviceBusDataPayloadHd } from "../../device.dts";
-import { CmdId } from "../coders/dev-bin-json/cmd";
-import { ICmdHead } from "../coders/dev-bin-json/cmd-head";
-import { IPldTable, PldTable } from "../coders/dev-bin-json/pld-table";
 import { IRfirCoder, RfirCoder } from "../coders/rfir/rfir-coder";
 import { Debuger, INDDevice, NDDevice } from "../nd-device";
 
@@ -19,46 +15,46 @@ export  class RFIRDevice extends NDDevice implements IRFIRDevice {
         Debuger.Debuger.log("RFIRDEvice init");
     }
     
-    on_south_input_decode(p_hd: ICmdHead, p_pld: IPldTable): IDeviceBusDataPayload {
-        Debuger.Debuger.log("RFIRDEvice on_south_input_decode");
-        const payload = super.on_south_input_decode(p_hd, p_pld);
-        if (!payload) return;
+    // _on_south_input_decode(p_hd: ICmdHead, p_pld: IPldTable): IDeviceBusDataPayload {
+    //     Debuger.Debuger.log("RFIRDEvice on_south_input_decode");
+    //     const payload = super.on_south_input_decode(p_hd, p_pld);
+    //     if (!payload) return;
 
-        const hd = payload.hd;
-        const pld = payload.pld;
-        if (hd.cmd_id == CmdId.rfir_sniff) {  
-            const data = this.recvcmd.payload.tables[PldTable.Keys.rfir_sniff_data] as Buffer;    
-            if (data) {
-                pld[PldTable.Keys.rfir_sniff_data] = this.rfir_coder.decode(data);
-            }
-        } 
+    //     const hd = payload.hd;
+    //     const pld = payload.pld;
+    //     if (hd.cmd_id == CmdId.rfir_sniff) {  
+    //         const data = this.recvcmd.payload.tables[PldTable.Keys.rfir_sniff_data] as Buffer;    
+    //         if (data) {
+    //             pld[PldTable.Keys.rfir_sniff_data] = this.rfir_coder.decode(data);
+    //         }
+    //     } 
 
-        return {
-            hd: hd,
-            pld: pld
-        };
-    }
+    //     return {
+    //         hd: hd,
+    //         pld: pld
+    //     };
+    // }
 
-    on_north_input_encode(p_hd: IDeviceBusDataPayloadHd, p_pld: {}): IDeviceBusDataPayload {
-        const payload = super.on_north_input_encode(p_hd, p_pld);
-        if (!payload) return;
-        const hd = payload.hd;
-        let pld = payload.pld;
+    // _on_north_input_encode(p_hd: IDeviceBusDataPayloadHd, p_pld: {}): IDeviceBusDataPayload {
+    //     const payload = super.on_north_input_encode(p_hd, p_pld);
+    //     if (!payload) return;
+    //     const hd = payload.hd;
+    //     let pld = payload.pld;
 
 
-        const bytess = pld && pld[PldTable.Keys.rfir_send_data];
-        if (bytess) {            
-            const buf = this.rfir_coder.encode(bytess);
+    //     const bytess = pld && pld[PldTable.Keys.rfir_send_data];
+    //     if (bytess) {            
+    //         const buf = this.rfir_coder.encode(bytess);
 
-            if (buf && buf.length > 0) {
-                pld = {};
-                pld[PldTable.Keys.rfir_send_data] = buf;   
-            } 
-        }             
+    //         if (buf && buf.length > 0) {
+    //             pld = {};
+    //             pld[PldTable.Keys.rfir_send_data] = buf;   
+    //         } 
+    //     }             
         
-        return {
-            hd: hd,
-            pld: pld
-        }
-    }    
+    //     return {
+    //         hd: hd,
+    //         pld: pld
+    //     }
+    // }    
 }
