6006c08632311719bc4ae9deca18e8573da0b7b2
陈志谦 <761208@nd.com>
2023-02-28 16:43:55 +0800

    fire-qtl-tx3016a: add events: cus_fire/cus_op/cus_test

--- src/device/amd/fire/qtl-tx3016a/device.ts
+++ src/device/amd/fire/qtl-tx3016a/device.ts
@@ -3,7 +3,7 @@ import { IDeviceBaseAttr, IDeviceBusDataPayloadHd, IDeviceBusEventData } from ".
 import { Cmd, CmdId } from "../../coders/dev-bin-json/cmd";
 import { PldTable } from "../../coders/dev-bin-json/pld-table";
 import { INDDevice, NDDevice } from "../../nd-device/device";
-import { DataTable, IDataTable_App_Info_Status } from "./datatable";
+import { DataTable, IDataTable_App_Info_Op, IDataTable_App_Info_Status } from "./datatable";
 
 export type IQTL_TX3016A = INDDevice
 
@@ -31,41 +31,82 @@ export class QTL_TX3016A extends NDDevice implements IQTL_TX3016A {
         const data = pld[PldTable.Keys.penet_data];
         const dataTable = DataTable.decode(data);
 
-        hd.entry.id = CmdId.Names[CmdId.report];
-  
-
-        //这里检查是否部件运行状态上报：control.cmd=2(发送数据), app.type=2(上传部件运行状态)，然后单独上报部件状态;
+        //这里检查控制单元命令：control.cmd=2(发送数据), app.type=2(部件运行状态)/4(设备操作信息)/9(通信线路上行测试)
         dataTable.forEach(value => {
+            //发送数据
             if (value && value.control && value.control.cmd == 2) {
+                //上传火灾自动报警系统部件运行状态
                 if (value.app && value.app.type == 2 && value.app.infos) {
                     value.app.infos.forEach(info  => {
                         info = (info as IDataTable_App_Info_Status);
                         if (info.unit_addr) {
-                            const sub_pref = this.attrs.sub_pref ? this.attrs.sub_pref: "";
-                            const id = sub_pref + info.unit_addr;
-                            const device: IDeviceBaseAttr = Object.assign({}, this.attrs);
-                            device.id = id;
-                            device.pid = this.attrs.id;
-                            device.model = this.attrs.model + "-UNIT";
-                            device.name = (info.unit_mark ? info.unit_mark : "") + (info.unit_addr_desc ? info.unit_addr_desc : "");
-                            device.nick = device.name;
-                            device.desc = device.name;
-
+                            const _hd: IDeviceBusDataPayloadHd = {
+                                entry:{
+                                    type: "evt",
+                                    id: "cus_fire"
+                                },
+                                stp: 0
+                            }
                             
-                            const msg: IDeviceBusEventData = {
-                                id: sub_pref + info.unit_addr,
+                            const _msg: IDeviceBusEventData = {
+                                id: msg.id,
                                 payload: {
-                                    hd: hd,
+                                    hd: _hd,
                                     pld: info,
-                                    device: {
-                                        attrs: device
-                                    },
+                                    device: msg.device
                                 },
                                 
                             }
-                            this.events.north.output.emit(msg);
+                            this.events.north.output.emit(_msg);
                         }
                     })
+                }
+
+                //上传火灾自动报警系统设备操作信息
+                if (value.app && value.app.type == 4 && value.app.infos) {
+                    value.app.infos.forEach(info  => {
+                        info = (info as IDataTable_App_Info_Op);
+                            const _hd: IDeviceBusDataPayloadHd = {
+                                entry:{
+                                    type: "evt",
+                                    id: "cus_op"
+                                },
+                                stp: 0
+                            }
+                            
+                            const _msg: IDeviceBusEventData = {
+                                id: msg.id,
+                                payload: {
+                                    hd: _hd,
+                                    pld: info,
+                                    device: msg.device
+                                },
+                                
+                            }
+                            this.events.north.output.emit(_msg);
+                    })
+                }
+
+                //通信线路上行测试
+                if (value.app && value.app.type == 9) {
+                    const _hd: IDeviceBusDataPayloadHd = {
+                        entry:{
+                            type: "evt",
+                            id: "cus_test"
+                        },
+                        stp: 0
+                    }
+                    
+                    const _msg: IDeviceBusEventData = {
+                        id: msg.id,
+                        payload: {
+                            hd: _hd,
+                            pld: {},
+                            device: msg.device
+                        },
+                        
+                    }
+                    this.events.north.output.emit(_msg);
                 }
             }                
         })
